750f6c707b7e23a9da16d61ca3cb58ddef72a81a
trcGitHub <github@turcom.com.tr>
2019-05-02 17:41:40 +0300

    Operating Systems Folders are added

--- Ubuntu/setup.sh
+++ Ubuntu/setup.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade
 sudo apt-get install kodi
 sudo apt-get install kodi-pvr-tvheadend-hts
 mkdir -p ~/.kodi/userdata/addon_data/pvr.hts
-sudo cp media/* /usr/share/kodi/media/
-sudo cp textures/Textures.xbt /usr/share/kodi/addons/skin.estuary/media/
-sudo cp pvr/pvr.hts/settings.xml ~/.kodi/userdata/addon_data/pvr.hts/
+sudo cp Ubuntu/media/* /usr/share/kodi/media/
+sudo cp Ubuntu/textures/Textures.xbt /usr/share/kodi/addons/skin.estuary/media/
+sudo cp Ubuntu/pvr/pvr.hts/settings.xml ~/.kodi/userdata/addon_data/pvr.hts/
 echo "alias trctv='kodi'" >> ~/.bashrc
