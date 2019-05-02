41114a7fd18fcd517bcb046a0c8571b934e5e732
Selcuk Yolcu <yolcus@turcom.com.tr>
2019-05-02 17:00:00 +0300

    project files is created

---
+++ README.txt
@@ -0,0 +1,21 @@
+TRCTV IPTV AD BLOCKING PROJECT
+------------------------------
+This package is to install IPTV framework.
+
+Installation instruction:
+	1- Open Ubuntu Terminal
+	2- Type:
+	   cd path/to/trctv && ./setup.sh
+	3- After succesful installation, 
+	   Open a new terminal,
+	   Type:
+	   trctv
+	4- Click below link to enable TVheadend,
+	   On Left bar,
+	   Add-ons -> My add-ons -> PVR clients -> Tvheadend HTSP Client -> Enable
+	   And ESC for Home Page
+	ENJOY!
+
+Turcom Technology
+Software Team
+2019
---
+++ setup.sh
@@ -0,0 +1,12 @@
+echo "Kodi installation is starting ..."
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:team-xbmc/ppa
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install kodi
+sudo apt-get install kodi-pvr-tvheadend-hts
+mkdir -p ~/.kodi/userdata/addon_data/pvr.hts
+sudo cp media/* /usr/share/kodi/media/
+sudo cp textures/Textures.xbt /usr/share/kodi/addons/skin.estuary/media/
+sudo cp pvr/pvr.hts/settings.xml ~/.kodi/userdata/addon_data/pvr.hts/
+echo "alias trctv='kodi'" >> ~/.bashrc
